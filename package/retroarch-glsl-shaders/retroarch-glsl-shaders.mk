c62d21fc1a5f0949fe1c99471b0835b8783ba842
05warmups.delayer <05warmups.delayer@icloud.com>
2025-01-24 17:59:58 -0400

    retroarch fixes

--- package/retroarch-glsl-shaders/retroarch-glsl-shaders.mk
+++ package/retroarch-glsl-shaders/retroarch-glsl-shaders.mk
@@ -9,8 +9,16 @@ RETROARCH_GLSL_SHADERS_SITE = https://github.com/libretro/glsl-shaders.git
 RETROARCH_GLSL_SHADERS_SITE_METHOD = git
 RETROARCH_GLSL_SHADERS_DEPENDENCIES = host-pkgconf
 
+# Sharp Shimmerless Shader repository URL
+SHARP_SHIMMERLESS_REPO = https://github.com/Woohyun-Kang/Sharp-Shimmerless-Shader.git
+
 define RETROARCH_GLSL_SHADERS_INSTALL_TARGET_CMDS
+	# Install the existing RetroArch GLSL shaders
 	$(TARGET_MAKE_ENV) $(MAKE) INSTALLDIR=$(TARGET_DIR)/root/.config/retroarch/shaders -C $(@D) install
+	
+	# Clone the Sharp Shimmerless Shader repository
+	mkdir -p $(TARGET_DIR)/root/.config/retroarch/shaders/sharp-shimmerless
+	git clone $(SHARP_SHIMMERLESS_REPO) $(TARGET_DIR)/root/.config/retroarch/shaders/sharp-shimmerless
 endef
 
 $(eval $(generic-package))
